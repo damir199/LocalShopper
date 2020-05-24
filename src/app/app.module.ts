bd9cd58360b3433430e3611fbc56590c228133e7
Damir <damir8520@gmail.com>
2020-05-24 02:04:09 +0100

    generated store and product components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { ProductComponent } from './components/product/product.component';
+import { StoreComponent } from './components/store/store.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductComponent,
+    StoreComponent,
 
 
   ],
