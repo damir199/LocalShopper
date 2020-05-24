1b7e207127f5693e965e1ed6a6e4ddc98e118714
Damir <damir8520@gmail.com>
2020-05-24 01:37:58 +0100

    generated and prepared all o fthe auth section. -authguard -service -login/register components -login service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
+    LoginComponent,
+    RegisterComponent,
 
   ],
   imports: [
