d8479514a4b602b1a355ca6ff74967e3fde4b94c
Damir <damir8520@gmail.com>
2020-05-24 02:09:42 +0100

    basic routes setup for main components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
+import { LoginComponent } from './auth/login/login.component';
+import { registerLocaleData } from '@angular/common';
+import { RegisterComponent } from './auth/register/register.component';
+import { VendorRegistrationComponent } from './auth/vendor-registration/vendor-registration.component';
+import { StoreComponent } from './components/store/store.component';
 
 
 const routes: Routes = [
   {
     path: "",
     component: DefaultComponent,
+    children: [
+      {
+        path: "login",
+        component: LoginComponent,
+      },
+      {
+        path: "register",
+        component: RegisterComponent,
+      },
+      {
+        path: "vendor-registration",
+        component: VendorRegistrationComponent,
+      },
+      {
+        path: "store",
+        component: StoreComponent,
+      },
+    ]
   },
 
 ];
