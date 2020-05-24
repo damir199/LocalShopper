a3e3a0447acae239275faceebfb5616545390aaf
Damir <damir8520@gmail.com>
2020-05-24 02:06:39 +0100

    sorted out shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
-import { ProductComponent } from './components/product/product.component';
-import { StoreComponent } from './components/store/store.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    StoreComponent,
+
 
 
   ],
--- src/app/shared/shared/shared.module.ts
+++ src/app/shared/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
-import { AngularMaterialModule } from 'src/app/modules/angular-material/angular-material.module';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { AngularMaterialModule } from 'src/app/modules/angular-material/angular-material.module';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
-import { LoginComponent } from 'src/app/auth/login/login.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+
 import { RegisterComponent } from 'src/app/auth/register/register.component';
+import { LoginComponent } from 'src/app/auth/login/login.component';
 import { ErrorComponent } from 'src/app/error/error/error.component';
 import { VendorRegistrationComponent } from 'src/app/auth/vendor-registration/vendor-registration.component';
+import { ProductComponent } from 'src/app/components/product/product.component';
+import { StoreComponent } from 'src/app/components/store/store.component';
 
 
 
@@ -23,7 +27,9 @@ import { VendorRegistrationComponent } from 'src/app/auth/vendor-registration/ve
     RegisterComponent,
     SearchBarComponent,
     ErrorComponent,
-    VendorRegistrationComponent
+    VendorRegistrationComponent,
+    ProductComponent,
+    StoreComponent
 
 
   ],
@@ -44,7 +50,9 @@ import { VendorRegistrationComponent } from 'src/app/auth/vendor-registration/ve
     RegisterComponent,
     SearchBarComponent,
     ErrorComponent,
-    VendorRegistrationComponent
+    VendorRegistrationComponent,
+    ProductComponent,
+    StoreComponent
 
   ],
 })
