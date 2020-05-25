63a372ca2cb484fa94d41c42f964e748b3171e01
Damir <damir8520@gmail.com>
2020-05-26 00:20:05 +0100

    created vendors interface. started working on location service.

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { iVendor } from "../interfaces/ivendor";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  private vendorsData: iVendor[];
+  private location: string;
+  private currentlocation: string;
+
+  getVendors(postcode: string) {
+    return { ...this.vendorsData.find(p => p.postcode == postcode) }
+  }
 }
